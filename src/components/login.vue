8f17005b71d995e9d13f082e89302bc5eb15d9fc
haoxin <haoxin@youerdian.com>
2020-02-20 20:54:37 +0800

    添加了axios和element插件

---
+++ src/components/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login_wrapper">
+    <el-form :model="loginRuleForm" :rules="loginRuleFormRules" ref="loginRuleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="活动名称" prop="name">
+        <el-input v-model="loginRuleForm.name"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+      loginRuleForm: {
+        username: 'admin',
+        password: 123456
+      },
+      loginRuleFormRules: {}
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+  .login_wrapper{}
+</style>
