4f79a0780619bb2161c00c0e98a5e29ba517f380
czx <5602705+czx-2019@user.noreply.gitee.com>
2020-12-02 15:43:43 +0800

    添加了测试组件

---
+++ src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <login></login>
+</template>
+
+<script>
+import Login from '../components/login'
+export default {
+  components: {
+    Login
+  },
+  props: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+
+</style>
